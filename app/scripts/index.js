3b38666ea82c1ba0fa4c94bbe0badfa904eafa71
BasitCode <53387349+BasitCode@users.noreply.github.com>
2019-08-13 19:06:32 +0500

    update

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,24 +7,35 @@ let {
     dialog
 } = remote
 let isDev = require('electron-is-dev')
-let Vue = require(isDev ? '../scripts/vue.dev' : '../scripts/vue.js')
+//let Vue = require('../scripts/vue.dev.js')
+let Vue = require('../scripts/vue.js')
+let vueMessage = require('../scripts/vue-message')
 let vtj = require('vue-to-js')
 let path = require('path')
 
+Vue.use(vueMessage, {
+    messageText:'message test',
+    duration: 3000,
+    background: 'rgba(0,0,0,0.5)'
+})
 
 let vueApp = new Vue({
     el: '#root',
     data() {
         return {
             files: [],
-            mode: 'umd'
+            mode: 'umd',
+            msg : 'message'
         }
+    },
+    components: {
+        'vue-message':vueMessage
     },
     methods: {
         Compile() {
             this.files.forEach(file => {
-                file.status = ''
                 try {
+                    file.status = 'compiling'
                     vtj.compile({
                         resource: file.path,
                         dest: path.dirname(file.path),
@@ -35,6 +46,13 @@ let vueApp = new Vue({
                     file.status = 'fail'
                     console.warn(e);
                 }
+            })
+
+            this.UpdateStorage()
+            this.$msg({text:'未曾遗忘的青春', background: 'red'})
+            shell.beep()
+            let compileNotification = new Notification('Vue Compiler', {
+                body: 'Compile proccess finished'
             })
         },
         LoadFiles() {
@@ -57,33 +75,49 @@ let vueApp = new Vue({
                         status: ''
                     })
                 })
+                this.UpdateStorage()
             }
+        },
+        UpdateStorage() {
+            localStorage.setItem('files', JSON.stringify(this.files))
         },
         ShowFile(path) {
             shell.showItemInFolder(path)
         },
         RemoveFiles() {
             this.files = []
+            this.UpdateStorage()
         },
         RemoveFile(index) {
             this.files.splice(index, 1)
+            this.UpdateStorage()
         },
         FileStatus(index) {
             let status = ''
             if (this.files[index]) {
                 if (this.files[index].status == 'success')
                     status = ' positive'
+                else if (this.files[index].status == 'compiling')
+                    status = ' compiling'
                 else if (this.files[index].status == 'fail')
                     status = ' negative'
 
             }
             return status
+        },
+        CloseApp() {
+            app.quit()
         }
     },
     computed: {
-        windowTitle : function () {
-            return `${app.getName()} (${app.getVersion()})`  
+        windowTitle: function () {
+            return `${app.getName()} (${app.getVersion()})`
         }
+    },
+    mounted() {
+        let files = JSON.parse(localStorage.getItem('files'))
+        if (Array.isArray(files))
+            this.files = files
     }
 })
 
@@ -101,6 +135,7 @@ document.body.ondrop = (ev) => {
                 status: ''
             })
         })
+        vueApp.UpdateStorage()
     } catch (e) {
         console.log(e);
     }
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function createWindow () {
   win = new BrowserWindow({ width: 800, height: 600, frame:false })
 
   win.loadFile('app/views/index.html')
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
   win.on('closed', () => {
     win = null
   })
@@ -19,7 +19,7 @@ function createWindow () {
 app.on('ready', () => {
   if (process.env.NODE_ENV !== 'production') {
     createWindow()
-    require('vue-devtools').install()
+    //require('vue-devtools').install()
   }
 })
 
